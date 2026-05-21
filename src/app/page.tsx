4121038c397ee2d2f571dfb20cbf818be80ab76e
Zenara Tools <zenaratools@gmail.com>
2026-05-21 09:54:47 +0800

    Replace Phone icon with custom SVG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -889,8 +889,15 @@ function LandingPage() {
                 {/* Phone */}
                 <div className="flex gap-6 text-white">
                   <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center h-14 w-14 bg-[#151921] text-[#d4af37] shadow-xl border border-white/5">
-                      <Phone size={24} strokeWidth={2} />
+                    <div className="flex items-center justify-center h-14 w-14 bg-[#151921] shadow-xl border border-white/5">
+                      <img
+                        src="/phone.svg"
+                        alt=""
+                        width={32}
+                        height={32}
+                        className="h-8 w-8 object-contain"
+                        aria-hidden="true"
+                      />
                     </div>
                   </div>
                   <div>
